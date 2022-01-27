1593b1b30e0cec9b0b33d968daa350f00502557f
Galitan-dev <galitan.dev@gmail.com>
2022-01-27 20:48:59 +0100

    fix and generators

--- src/create_command.sh
+++ src/create_command.sh
@@ -1,8 +1,15 @@
 PROJECT_PATH="${GABUM_DIRECTORY}/${args[name]}"
 
 git init "${PROJECT_PATH}"
-cd "${PROJECT_PATH}"
+cd "${PROJECT_PATH}" 
 
-gh repo create
+license-generator --project "${args[name]}" --year $(date "+%Y") ${args[license]}
+yo standard-readme
+
+if [[ $private ]]; then
+    gh repo create "${args[name]}" --private -s "${PROJECT_PATH}"
+else
+    gh repo create "${args[name]}" --public -s "${PROJECT_PATH}"
+fi
 
 code -n "${PROJECT_PATH}"
--- src/initialize.sh
+++ src/initialize.sh
@@ -4,8 +4,7 @@ else
   GABUM_HOME="${GABUM_HOME}"
 fi
 
-if [[ ! -f "${GABUM_HOME}" ]]
-then
+if [[ ! -f "${GABUM_HOME}" ]]; then
     mkdir -p "${GABUM_HOME}"
 fi
 
@@ -18,4 +17,41 @@ fi
 if [[ ! -f "${GABUM_DIRECTORY}" ]]
 then
     mkdir -p "${GABUM_DIRECTORY}"
+fi
+
+if ! [ -x "$(command -v git)" ]; then
+  echo 'Error: git is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v cargo)" ]; then
+  echo 'Error: cargo is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v npm)" ]; then
+  echo 'Error: npm is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v license-generator)" ]; then
+  echo 'Installing license-generator'
+  cargo install license-generator
+fi
+
+if ! [ -x "$(command -v yo)" ]; then
+  echo 'Installing yeoman'
+  npm install --global yo
+fi
+
+if [[ ! -f "${GABUM_DIRECTORY}/.cache" ]]; then
+  mkdir -p "${GABUM_DIRECTORY}/.cache"
+fi
+
+if [[ ! -f "${GABUM_DIRECTORY}/.cache/standard-readme" ]]; then
+  echo 'Installing generator-readme-md'
+  export npm_config_loglevel=silent
+  npm install --global yo generator-standard-readme
+  unset npm_config_loglevel
+  touch "${GABUM_DIRECTORY}/.cache/standard-readme"
 fi
